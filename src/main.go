0b759444676072bc82d060bd32f2cbcd248e0e4b
Sandro Lain <sandrolain@outlook.com>
2023-04-10 21:58:03 +0200

    Fixes for steps build

--- src/main.go
+++ src/main.go
@@ -1,5 +1,31 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"encoding/json"
+	"os"
 
+	"github.com/sandrolain/go-ci-example/src/client"
+)
+
+func main() {
+	c, err := client.NewDbClient(os.Getenv("MONGODB_URI"), os.Getenv("MONGODB_DB"))
+	if err != nil {
+		panic(err)
+	}
+
+	reader := bufio.NewReader(os.Stdin)
+	var data []byte
+	_, err = reader.Read(data)
+	if err != nil {
+		panic(err)
+	}
+
+	var movies []client.Movie
+	err = json.Unmarshal(data, &movies)
+	if err != nil {
+		panic(err)
+	}
+
+	err = c.InsertMovies(movies)
 }
